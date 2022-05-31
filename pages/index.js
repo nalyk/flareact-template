125a10909137acfa9e939f5fbba1d665c59be255
Ion Nalyk Calmîș <nalyk@yoda.md>
2022-05-31 18:40:17 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Button from '@mui/material/Button';
+
 export default function Index() {
   return (
-    <h1>
-      You're running React on the Edge!
-    </h1>
+    <>
+      <h1>
+        You're running React on the Edge!
+      </h1>
+      <Button variant="contained">Hello World</Button>
+    </>
   );
 }
