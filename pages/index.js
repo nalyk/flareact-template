2c01875d48515981cc8e52613a0ce0538d065b71
Ion Calmîș <dev.ungheni@gmail.com>
2022-06-01 19:53:24 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Button from '@mui/material/Button';
-
 export default function Index() {
   return (
-    <>
       <h1>
-        You're running React on the Edge!
+        React on CF Edge
       </h1>
-      <Button variant="contained">Hello World</Button>
-    </>
   );
 }
